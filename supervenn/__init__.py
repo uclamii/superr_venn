2efd74ed3e61fe1a41b01ade57e2db11ff18e997
Feng <jeffreyfeng@REDLRADADM14958.ad.medctr.ucla.edu>
2024-04-09 10:50:11 -0700

    expose break_into_chunks

--- supervenn/__init__.py
+++ supervenn/__init__.py
@@ -1,6 +1,7 @@
 from supervenn._algorithms import (
     get_chunks_and_composition_array,
-    get_permutations
+    get_permutations,
+    break_into_chunks
 )
 
 from supervenn._plots import supervenn
